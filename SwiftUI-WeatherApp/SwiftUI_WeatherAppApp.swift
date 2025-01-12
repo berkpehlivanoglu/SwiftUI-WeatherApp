55993a23e62f9b69cc4ba4a78b5d3b4b1b5fc43d
Berk <berk.pehlivanoglu@migrosonline.com>
2025-01-12 11:23:45 +0300

    Initial commit

---
+++ SwiftUI-WeatherApp/SwiftUI_WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherAppApp.swift
+//  SwiftUI-WeatherApp
+//
+//  Created by Berk Pehlivanoğlu on 11.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
